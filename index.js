3e8609f28de3623a9629a0b22116e0922843ed62
Svetlana <svetafrontit@tutanota.com>
2021-02-01 20:44:41 +0200

    fixed

--- index.js
+++ index.js
@@ -6,17 +6,25 @@ class Accordeon {
     setHeight() {
         const titles = this.container.querySelectorAll('.title');
         for (let i = 0; i < titles.length; i++) {
+            
             titles[i].addEventListener("click", (event) => {
+                titles.forEach(title => {
+                    if(title != event.target){
+                        title.classList.remove('active');
+                        title.nextElementSibling.style.maxHeight = null;
+                    };
+                });
                 event.target.classList.toggle("active");
                 const body = titles[i].nextElementSibling;
             if (body.style.maxHeight){
                 body.style.maxHeight = null;
               } else {
                 body.style.maxHeight = body.scrollHeight + "px";
-            } 
-            });
-            
+            }; 
+            });   
+                    
         }
+        
     }
 }
 new Accordeon(document.getElementById('list'));
