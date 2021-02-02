cdbd6d28635525617a29abcae7d908c70cb900a3
Svetlana <svetafrontit@tutanota.com>
2021-02-02 09:00:10 +0200

    make it prettier

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 class Accordeon {
-    constructor (container) {
-        this.container = container;
-        this.setHeight()
-    }
-    setHeight() {
-        const titles = this.container.querySelectorAll('.title');
-        for (let i = 0; i < titles.length; i++) {
-            titles[i].addEventListener("click", (event) => {
-                titles.forEach(title => {
-                    if(title != event.target){
-                        title.classList.remove('active');
-                        title.nextElementSibling.style.maxHeight = null;
-                    };
-                });
-                event.target.classList.toggle("active");
-                const body = titles[i].nextElementSibling;
-            if (body.style.maxHeight){
-                body.style.maxHeight = null;
-              } else {
-                body.style.maxHeight = body.scrollHeight + "px";
-            }; 
-            });   
-                    
+  constructor(container) {
+    this.container = container;
+    this.setHeight();
+  }
+  setHeight() {
+    const titles = this.container.querySelectorAll(".title");
+    for (let i = 0; i < titles.length; i++) {
+      titles[i].addEventListener("click", (event) => {
+        titles.forEach((title) => {
+          if (title != event.target) {
+            title.classList.remove("active");
+            title.nextElementSibling.style.maxHeight = null;
+          }
+        });
+        event.target.classList.toggle("active");
+        const body = titles[i].nextElementSibling;
+        if (body.style.maxHeight) {
+          body.style.maxHeight = null;
+        } else {
+          body.style.maxHeight = body.scrollHeight + "px";
         }
-        
+      });
     }
+  }
 }
-new Accordeon(document.getElementById('list'));
-        
+new Accordeon(document.getElementById("list"));
